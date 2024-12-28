4fbef69eab71497c460e444ce936eed6c362512d
Anggia Intan <intananggia23@gmail.com>
2024-12-28 20:35:45 +0700

    Initial commit

---
+++ app/src/main/java/repository/MahasiswaRepository.kt
@@ -0,0 +1,48 @@
+package repository
+
+import model.Mahasiswa
+import okio.IOException
+import service.MahasiswaService
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa (mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa (nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa (nim: String)
+    suspend fun getMahasiswaByNim (nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful)
+                throw IOException(
+                    "Failed to delete mahasiswa. HTTP Status code: " +
+                            "${response.code()}")
+        } else {
+            response.message()
+            println(response.message())
+        }
+        } catch (e.Exception) {
+            throw e
+        }
+    }
+
+override suspend fun getMahasiswa(): List<Mahasiswa> =
+    mahasiswaApiService.getAllMahasiswa()
+
+override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+    return mahasiswaApiService.getMahasiswabyNim(nim)
+}
+}
