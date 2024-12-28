063766e662b8beb788927207437cd63c4044b1c4
Anggia Intan <intananggia23@gmail.com>
2024-12-28 23:32:37 +0700

    membuat data class InsertUiEvent

---
+++ app/src/main/java/com/example/api/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.api.ui.viewmodel
+
+data class InsertUiEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
