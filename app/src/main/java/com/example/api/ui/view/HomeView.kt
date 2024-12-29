cd4c23088db9439af6981dedcfc396c0027f487e
Anggia Intan <intananggia23@gmail.com>
2024-12-29 12:47:54 +0700

    membuat function OnLoading

--- app/src/main/java/com/example/api/ui/view/HomeView.kt
+++ app/src/main/java/com/example/api/ui/view/HomeView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -29,6 +30,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.api.R
 import model.Mahasiswa
+
+@Composable
+fun OnLoading (modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(id = R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
 
 @Composable
 fun OnError(retryAction:() -> Unit, modifier: Modifier = Modifier) {
